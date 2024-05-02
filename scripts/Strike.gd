3d17baa98d68f4fc92a278b8c91604461cab72d2
raczynskid <raczynski90@gmail.com>
2024-05-02 12:02:46 +0200

    FSM set up for player movement and attack sequence

--- scripts/Strike.gd
+++ scripts/Strike.gd
@@ -4,21 +4,42 @@ extends State
 var idle_state : State
 @export
 var walk_state : State
+@export
+var next_strike : State
 
 var is_done : bool = false
+var continue_strike : bool = false
+
+var cooldown_setting = 0.2
+var cooldown = cooldown_setting
 
 func enter() -> void:
 	super()
+	is_done = false
+	continue_strike = false
 
 func exit() -> void:
-	is_done = false
+	# reset cooldown on state end
+	cooldown = cooldown_setting
 
 func process_physics(delta: float) -> State:
-	if is_done:
-		return walk_state
-	else:
-		return null
+	# cound down ticker to make next strike possible
+	if cooldown > 0:
+		cooldown -= delta
 
+	# on animation end, check if next attack has been queued
+	if is_done:
+		# if attack queued, proceed to next strike
+		if continue_strike:
+			return next_strike
+		# if no attack queued, return to idle
+		return walk_state
+	
+	# every frame, check if attack button pressed after ticker elapsed
+	if Input.is_action_just_pressed("ui_accept") and cooldown <= 0:
+		# if attack pressed in time window, queue next attack
+		continue_strike = true
+	return null
 
 func _on_animated_sprite_2d_animation_finished():
 	is_done = true
